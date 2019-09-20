14ebea31a111663e2565bb0df0fdff9d962a93d1
haein <kendy93@sbcn.co.kr>
2019-09-21 00:18:10 +0900

    투두리스트 기능 구현 완료

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
 class Himentum {
-  constructor(apiKey) {
+  constructor(apiKey,todoList,todoLastId,userName) {
     this.wheatherApiKey = apiKey.OPEN_WEATHER_API_KEY
     this.imageApiKey = apiKey.UNSPLASH_ACCESS_KEY
     this.datetime = new Date()
     this.geolocation = {}
     this.weater = {}
+    this.todoLastId = todoLastId || 0
+    this.todoList = todoList
+    this.userName = userName
     this.date = document.getElementsByClassName('date')[0]
     this.time = document.getElementsByClassName('time')[0]
     this.notice = document.getElementsByClassName('notice')[0]
     this.location = document.getElementsByClassName('location')[0]
     this.temp = document.getElementsByClassName('temp')[0]
+    this.hello =  document.getElementsByClassName('hello')[0]
+    this.todoListContainer =  document.getElementsByClassName('todoList')[0]
+    this.todoInput = document.getElementsByClassName('todoInput')[0]
     this.weatherType = document.getElementsByClassName('weatherType')[0]
     this.weatherIcon =  document.getElementsByClassName('weatherIcon')[0]
     this.copyright = document.getElementsByClassName('copyright')[0]
@@ -37,13 +43,13 @@ class Himentum {
         this.weater = {
           temp:data.main.temp,
           weatherType:data.weather[0].main,
-          weatherIcon:data.weather[0].icon
+          weatherIcon:data.weather[0].icon.substring(0,2)
         }
 
         this.location.textContent = `${data.name}, ${data.sys.country}`
         this.temp.innerHTML =`${Math.round(data.main.temp)}<span>°C</span>`
         this.weatherType.textContent = data.weather[0].main
-        this.weatherIcon.innerHTML = `<img src="./image/icons/${data.weather[0].icon}.png" alt="${data.weather[0].main}"/>`
+        this.weatherIcon.innerHTML = `<img src="./image/icons/${data.weather[0].icon.substring(0,2)}.png" alt="${data.weather[0].main}"/>`
         this.fetchBackgroundImage(data.weather[0].main)
       })
       .catch(err => {
@@ -91,16 +97,82 @@ class Himentum {
     },1000)
 
     this.getGeolocation()
+
+    if(this.userName){
+      this.getUserName()
+      this.getTodoList()
+      this.todoInput.classList.add('display');
+    }
   }
 
+  setTodoList({currentTarget}) {
+    this.todoList.push({
+      id : ++this.todoLastId ,
+      todo: currentTarget.value
+    })
+    localStorage.setItem('todoList' , JSON.stringify(this.todoList))
+    currentTarget.value = '';
+    this.getTodoList()
+  }
+
+  getTodoList() {
+    const html = this.todoList.map((v,i)=>{
+      return `<li><span>${v.todo}</span> <button type="button" class="deleteBtn" data-id="${v.id}">삭제</button></li>`
+    })
+    this.todoListContainer.innerHTML = html.join('')
+  }
+
+  deleteTodoList({target}) {
+    const targetId = +target.dataset.id;
+    const list = this.todoList.filter((v)=> targetId !== v.id)
+    this.todoList = list
+    localStorage.setItem('todoList' , JSON.stringify(this.todoList))
+    this.getTodoList()
+  }
+
+  setUserName({currentTarget}){
+    localStorage.setItem('userName' ,currentTarget.value)
+    this.getUserName()
+    this.todoInput.classList.add('display');
+  }
+  getUserName(){
+    const userName = localStorage.getItem('userName')
+    this.hello.textContent = `HELLO! ${userName}`;
+  }
 
 }
 
 (()=>{
-  const himentum = new Himentum({
-    OPEN_WEATHER_API_KEY,
-    UNSPLASH_ACCESS_KEY,
-    UNSPLASH_SECRET_KEY
+  const todoList = JSON.parse(localStorage.getItem('todoList')) || []
+  const todoLastId = (todoList.length === 0) ? 0 : todoList[todoList.length-1].id
+  const userName = localStorage.getItem('userName') || '' 
+
+  const himentum = new Himentum(
+    {
+      OPEN_WEATHER_API_KEY,
+      UNSPLASH_ACCESS_KEY,
+      UNSPLASH_SECRET_KEY,
+    },
+    todoList,
+    todoLastId,
+    userName,
+  );
+  const nameInput = document.getElementsByClassName('nameInput')[0]
+  if(nameInput){
+    nameInput.addEventListener("change",(e)=>{
+      himentum.setUserName(e)
+    });
+  }
+
+
+  const todoInput = document.getElementsByClassName('todoInput')[0]
+  todoInput.addEventListener("change",(e)=>{
+    himentum.setTodoList(e)
+  });
+
+  const todo = document.getElementsByClassName('todoList')[0]
+  todo.addEventListener('click',(e)=>{
+    himentum.deleteTodoList(e)
   });
 })()
 
